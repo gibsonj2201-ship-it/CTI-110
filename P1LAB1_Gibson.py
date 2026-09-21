9db528c5401ee8d9b5f0621960de9718b3d3bd59
gibsonj2201-ship-it <gibsonj2201@student.faytechcc.edu>
2026-09-20 18:52:40 -0700

    Add files via upload

---
+++ P1LAB1_Gibson.py
@@ -0,0 +1,7 @@
+# Jason Gibson
+# P1LAB1
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello ", first_name, last_name, "!", "welcome to CTI-110!")
